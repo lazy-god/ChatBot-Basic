d81ad20ad2e4484d4e385d04776e395f4fcc17ec
Darsh Pratap <darshpratap@gmail.com>
2018-02-16 21:44:38 +0530

    Change 5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const
     apiai = require('apiai'),
     request = require('request'),
     config = require('config'),
-    crypto = require('crypto-js'),
+    // crypto = require('crypto-js'),
     app = express(),
     apiApp = apiai(config.get('clientaccesstoken'));
 
