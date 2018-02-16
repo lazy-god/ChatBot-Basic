24fc9279c326a6e9e3de4884ad0445eba9660e77
Darsh Pratap <darshpratap@gmail.com>
2018-02-16 16:34:40 +0530

    res Change

--- index.js
+++ index.js
@@ -98,11 +98,10 @@ function handleMessage(sender_psid, received_message) {
         })
 
         apiResponse.on('response', (res) => {
-            response = res;
+            response = res.fulfillment.messages[0].speech;
             console.log('Inside', res);
+            console.log('Inside', res.fulfillment.messages[0].speech);
         })
-
-        console.log(response);
 
         apiResponse.on('error', () => console.log("Error from api ai"))
 
