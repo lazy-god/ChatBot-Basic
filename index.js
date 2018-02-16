2341c2684ea5fd95547ee38a7c46345a8ab03b34
Darsh Pratap <darshpratap@gmail.com>
2018-02-16 18:27:12 +0530

    res Change 6

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.post('/webhook', (req, res) => {
 
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
+            if (entry.feed) {
+                return console.log(entry.feed)
+            }
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
